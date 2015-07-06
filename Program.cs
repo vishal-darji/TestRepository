7d829a8b2e2841f06932cadf132960499b6c628f
Vishal.Darji <vishal4005@gmail.com>
2015-07-06 23:36:12 +0530

    Adding the program for setting upper case for each word

--- Program.cs
+++ Program.cs
@@ -10,7 +10,27 @@ namespace GithubTest
         static void Main(string[] args)
         {
             //
-            Console.WriteLine("Test12d");
+            Console.WriteLine(ConvertString(Console.ReadLine()));
+            Console.Read();
+        }
+
+        public static string ConvertString(string text)
+        {
+            string[] str = text.Split(" ".ToCharArray());
+            StringBuilder builder = new StringBuilder();
+            foreach (string s in str)
+            {
+                Console.WriteLine(s);
+                if (!string.IsNullOrEmpty(builder.ToString()))
+                {
+                    builder.Append(" ");
+                    
+                }
+                
+                
+                builder.Append( char.ToUpper(s.ToCharArray()[0]) + s.Substring(1));
+            }
+            return builder.ToString();
         }
     }
 }
